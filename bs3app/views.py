2a9da16f83cd05f3d7e8890cf6a2cc0e54cd14e9
Janos Gyerik <info@janosgyerik.com>
2014-01-01 22:41:07 +0100

    cleaned up unused methods

--- bs3app/views.py
+++ bs3app/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render, render_to_response
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
 
 def home(request):
     return render_to_response('home.html')
-    #return HttpResponse('hello')
