a29eb32a8fe9fce8c5f446d6b3655463df09c4c6
Janos Gyerik <info@janosgyerik.com>
2014-01-02 22:19:58 +0100

    added Whitelisted model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Whitelisted(models.Model):
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return self.email
