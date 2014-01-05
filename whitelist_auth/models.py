f7a3cfd4a8f197b9e90dea142b5faba9630115ab
Janos Gyerik <info@janosgyerik.com>
2014-01-05 13:12:54 +0100

    added Whitelisted.get_last_login function

--- whitelist_auth/models.py
+++ whitelist_auth/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Whitelisted(models.Model):
@@ -6,6 +7,13 @@ class Whitelisted(models.Model):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
+
+    def get_last_login(self):
+        users = User.objects.filter(email=self.email).order_by('-last_login')
+        if users:
+            return users[0].last_login
+        else:
+            return 'never?'
 
     def summary(self):
         return '%s active=%s staff=%s super=%s' % (
