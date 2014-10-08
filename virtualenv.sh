5da795acca65ffa749680c94a1294280992efd63
Janos Gyerik <janos@kronos>
2014-10-08 21:41:44 +0200

    added helper scripts and ignore virtualenv

--- virtualenv.sh
+++ virtualenv.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 projectname=$(basename "$PWD")
-virtualenv=~/virtualenv/$projectname
+virtualenv=$(dirname "$BASH_SOURCE")/virtualenv
 if test -d $virtualenv; then
     . $virtualenv/bin/activate
 else
     echo virtualenv does not exist: $virtualenv >&2
     echo Create it with: virtualenv --distribute $virtualenv >&2
     echo Better yet, run ./scripts/init.sh >&2
-    return 1
 fi
